0278d3f724a586f53a1ac5b8064d30becf967dbe
bokushi <46466689+bokushi@users.noreply.github.com>
2024-05-20 16:08:43 +0900

    initial commit

---
+++ src/main/java/jp/co/dreamcareer/kubo/ia_kadai/IaKadaiApplication.java
@@ -0,0 +1,13 @@
+package jp.co.dreamcareer.kubo.ia_kadai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaKadaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaKadaiApplication.class, args);
+	}
+
+}
