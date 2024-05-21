ff4b286ef0d7b60feb6144da770116f87ce30a84
bokushi <46466689+bokushi@users.noreply.github.com>
2024-05-21 17:32:59 +0900

    フォームの結果を出力するページを追加

---
+++ src/main/java/jp/co/dreamcareer/kubo/ia_kadai/Forms.java
@@ -0,0 +1,61 @@
+package jp.co.dreamcareer.kubo.ia_kadai;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class Forms {
+    @NotBlank(message = "入力必須です")
+    private String lecturer;
+
+    @NotBlank(message = "入力必須です")
+    private String date;
+
+    @NotBlank(message = "入力必須です")
+    private String time;
+
+    @NotBlank(message = "入力必須です")
+    private String course;
+
+    @NotBlank(message = "入力必須です")
+    private String feedback;
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+}
--- src/main/java/jp/co/dreamcareer/kubo/ia_kadai/MyController.java
+++ src/main/java/jp/co/dreamcareer/kubo/ia_kadai/MyController.java
@@ -1,17 +1,26 @@
 package jp.co.dreamcareer.kubo.ia_kadai;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MyController {
     @GetMapping("/")
-    public String getSample() {
+    public String getSample(Model model) {
+        model.addAttribute("form", new Forms());
         return "index";
     }
 
-    @GetMapping("/result")
-    public String getResult() {
+    @PostMapping("/result")
+    public String getResult(@Validated @ModelAttribute("form") Forms form, BindingResult bindingResult, Model model) {
+        model.addAttribute("form", form);
+        System.out.println(form.getCourse());
+        System.out.println(form.getFeedback());
         return "result";
     }
 
