1b329937f25ea93491b95ec38dd8faf628fa9f85
bokushi <46466689+bokushi@users.noreply.github.com>
2024-05-23 10:28:11 +0900

    各ページに遷移するリンクを各ページに追加
    入力結果を表示するページにて講師名が正常に表示されなかった問題を修正

--- src/main/java/jp/co/dreamcareer/kubo/ia_kadai/FormDataRepository.java
+++ src/main/java/jp/co/dreamcareer/kubo/ia_kadai/FormDataRepository.java
@@ -3,5 +3,5 @@ package jp.co.dreamcareer.kubo.ia_kadai;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FormDataRepository extends JpaRepository<FormData, Integer> {
-
+    
 }
